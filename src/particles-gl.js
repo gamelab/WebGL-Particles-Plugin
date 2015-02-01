65eb19f92b9154e4b4de33eefd5f45bed1da58fb
Ben Richards <benjamindrichards@gmail.com>
2015-02-02 09:43:02 +1300

    Update KiwiJS version requirement to v1.2.1.

--- src/particles-gl.js
+++ src/particles-gl.js
@@ -23,9 +23,9 @@ Kiwi.Plugins.ParticlesGL = {
 	* @type String
 	* @public
 	*/
-	version:"1.2.0",
+	version:"1.2.1",
 
-	minimumKiwiVersion:"1.2.0",
+	minimumKiwiVersion:"1.2.1",
 
 	pluginDependencies: []
 };
